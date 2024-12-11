1e01f840fbd780c15621c5b5206eb344aedbc598
Immanuel Nwanevu V8 <speedymanspeed@gmail.com>
2024-12-11 17:57:11 +0100

    added a guide

--- files/js/script.js
+++ files/js/script.js
@@ -19,7 +19,7 @@ setInterval(updateClock, 1000);
 updateClock();
 
 function showPopup() {
-    alert("Please be advised Majority of these modules may only work for Transsion devices or specified Transsion sub-brands. We are not responsible for any damages caused upon the usage of these modules!");
+    alert("Please be advised Majority of these modules, ROMs and Recoveries may only work for Transsion devices or specified Transsion sub-brands. We are not responsible for any damages caused upon the usage of these Files!. Any Operation carried out using our files are completely based on the user's decision.");
 }
 
 
